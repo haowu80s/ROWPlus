aacd89e3b00daebc2d2fa358c0b529da472d21ff
wuhao <wuhao@stanford.edu>
2016-11-07 20:49:52 -0800

    Lorenz 96 model working with both ROWPlus and boost::odeint

---
+++ examples/Lorenz_96/lorenz96.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Hao Wu on 11/7/16.
+//
+
+#include "lorenz96.h"
+
+using namespace Eigen;
+using namespace ROWPlus;
+
+typedef BaseFunctor<double, 40, 40> BF;
+
+lorenz96::lorenz96(double F)
+    : m_F(F), m_wa1(InputType(inputs())), m_wa2(inputs()) {
+  eigen_assert(inputs() == values());
+};
+
+void lorenz96::operator()(const lorenz96::state_type &x,
+                          lorenz96::state_type &dxdt,
+                          const double _) {
+  for (int j = 2; j < inputs() - 1; ++j) {
+    dxdt[j] = -x[j - 1] * (x[j - 2] - x[j + 1]) - x[j] + m_F;
+  }
+  dxdt[0] = -x[inputs() - 1] * (x[inputs() - 2] - x[1]) - x[0] + m_F;
+  dxdt[1] = -x[0] * (x[inputs() - 1] - x[2]) - x[1] + m_F;
+  dxdt[inputs() - 1] =
+      -x[inputs() - 2] * (x[inputs() - 3] - x[0]) - x[inputs() - 1] + m_F;
+}
+
+DenseIndex lorenz96::f(double t, const Ref<const VectorXd> &x,
+                       VectorXd &v) {
+  for (int j = 2; j < inputs() - 1; ++j) {
+    v[j] = -x[j - 1] * (x[j - 2] - x[j + 1]) - x[j] + m_F;
+  }
+  v[0] = -x[inputs() - 1] * (x[inputs() - 2] - x[1]) - x[0] + m_F;
+  v[1] = -x[0] * (x[inputs() - 1] - x[2]) - x[1] + m_F;
+  v[inputs() - 1] =
+      -x[inputs() - 2] * (x[inputs() - 3] - x[0]) - x[inputs() - 1] + m_F;
+  return 0;
+};
+
+DenseIndex lorenz96::fdt(double t, const Ref<const VectorXd> &x,
+                         VectorXd &v) {
+  throw std::runtime_error("lorenz96::fdt() is not implemented.");
+  return -1;
+};
+
+DenseIndex lorenz96::df(double t,
+                        const Ref<const VectorXd> &x,
+                        MatrixXd &m) {
+  throw std::runtime_error("lorenz96::df() is not implemented.");
+  return -1;
+};
---
+++ examples/Lorenz_96/main.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Hao Wu on 11/7/16.
+//
+
+#include <iostream>
+#include "lorenz96.h"
+#include <boost/numeric/odeint.hpp>
+
+using namespace Eigen;
+using namespace ROWPlus;
+using namespace boost::numeric::odeint;
+
+int main() {
+  const double aTol = 1.0e-12;
+  const double rTol = 1.0e-4;
+  const double t1 = 0.3;
+  std::srand(101);
+  lorenz96 fun;
+  VectorXd x_1(40);
+  for (int i = 0; i < 40; ++i) {
+    x_1[i] = (double) (i - 20);
+  }
+  std::cout << "Lorenz-96 (N=40)" << std::endl;
+  ODEOptions<double> opts;
+  opts.relTol = rTol;
+  opts.absTol = aTol;
+  opts.h_max = 1e-2;
+  opts.h_init = 1e-5;
+  opts.TypeScheme = ROK4A;
+  opts.iWrite = true;
+  opts.iUserJac = false;
+  opts.maxKryDim = 4;
+  opts.minKryDim = 4;
+  opts.maxJacReuse = 0;
+  opts.maxSteps = 1000000;
+
+  ODESolver<ROWPlus::ODEJacSAP<lorenz96>, lorenz96> solver(&fun, opts);
+  ROWPlusSolverSpace::Status ret = solver.step(x_1, 0.0, t1);
+  assert(ret == ROWPlusSolverSpace::ComputeSucessful);
+
+  lorenz96::state_type x_2(40);
+  for (int i = 0; i < 40; ++i) {
+    x_2[i] = (double) (i - 20);
+  }
+
+  typedef runge_kutta_cash_karp54<lorenz96::state_type> error_stepper_type;
+  integrate_adaptive(make_controlled<error_stepper_type>(1.0e-12, 1.0e-12),
+                     fun, x_2, 0.0, t1, 1e-5 );
+  x_1 -= Map<VectorXd>(x_2.data(), 40);
+  cout << "error = "
+       << x_1.stableNorm()
+       << "/"
+       << Map<VectorXd>(x_2.data(), 40).stableNorm();
+  cout << " = " << x_1.stableNorm() / Map<VectorXd>(x_2.data(), 40).stableNorm()
+       << endl;
+  return 0;
+}
